0b090a2eb82594b30aea5ccb782d6362f5be2d97
cyon003 <aunghlainghtwe11@gmai.com>
2026-01-06 22:38:45 +0700

    no link

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import SubmitData from "./pages/SubmitData";
 export default function App() {
   return (
     <div>
-      <nav style={{ padding: 12 }}>
+      {/* <nav style={{ padding: 12 }}>
         <Link to="/">Submit Data</Link>
-      </nav>
-
+      </nav> */}
       <Routes>
         <Route path="/" element={<SubmitData />} />
+        
         {/* If your teacher wants a specific path, add it too: */}
-        <Route path="/submit-data" element={<SubmitData />} />
+        {/* <Route path="/submit-data" element={<SubmitData />} /> */}
       </Routes>
     </div>
   );
