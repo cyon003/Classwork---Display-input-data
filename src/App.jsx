d1adc57b13ba8fce1f3daa73a01fd0e41d2eca10
cyon003 <aunghlainghtwe11@gmai.com>
2026-01-06 22:44:40 +0700

    data change

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import SubmitData from "./pages/SubmitData";
 
 export default function App() {
   return (
-    <div>
-      {/* <nav style={{ padding: 12 }}>
-        <Link to="/">Submit Data</Link>
-      </nav> */}
-      <Routes>
-        <Route path="/" element={<SubmitData />} />
-        
-        {/* If your teacher wants a specific path, add it too: */}
-        {/* <Route path="/submit-data" element={<SubmitData />} /> */}
-      </Routes>
-    </div>
+    <Routes>
+    <Route path="/" element={<SubmitData />} />
+  </Routes>
+    // <div>
+    //   <nav style={{ padding: 12 }}>
+    //     <Link to="/">Submit Data</Link>
+    //   </nav>
+
+    //   <Routes>
+    //     <Route path="/" element={<SubmitData />} />
+    //     {/* If your teacher wants a specific path, add it too: */}
+    //     <Route path="/submit-data" element={<SubmitData />} />
+    //   </Routes>
+    // </div>
+
+   
   );
 }
